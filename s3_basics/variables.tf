9198ff5c5b5e88b3ccda59ea330890e25a92472f
subhasis020299 <subhasisdas125@gmail.com>
2022-05-19 17:36:21 +0530

    added object upload

--- s3_basics/variables.tf
+++ s3_basics/variables.tf
@@ -9,3 +9,15 @@ variable "bucket_prefix" {
   description = "The prefix to use for the S3 bucket name"
   default     = "tf-bucket"
 }
+
+variable "key_name" {
+  type        = string
+  description = "The key name to use for the S3 object"
+  default     = "tf-upload.jpg"
+}
+
+variable "source_path" {
+  type        = string
+  description = "The path to the file to upload to S3"
+  default     = "dog.jpg"
+}
