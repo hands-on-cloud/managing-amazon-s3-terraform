38356b38cba02654d069c934f3453a05d3706a83
subhasis020299 <subhasisdas125@gmail.com>
2022-05-26 21:39:21 +0530

    wesbite configuration for s3 done

---
+++ s3_website/s3.tf
@@ -0,0 +1,48 @@
+# create S3 bucket:
+resource "aws_s3_bucket" "bucket" {
+  bucket_prefix = var.bucket_prefix
+}
+
+#bucket must be public for website hosting:
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+    }
+  ]
+}
+EOF
+}
+
+# create S3 website hosting:
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document{
+    key = "error.html"
+  }
+}
+
+#upload website files to s3:
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.bucket.id
+
+  for_each = fileset("uploads/", "*")
+  key    = "website/${each.value}"
+  source = "uploads/${each.value}"
+  etag = filemd5("uploads/${each.value}")
+
+  depends_on = [
+    aws_s3_bucket.bucket
+  ]
+}
