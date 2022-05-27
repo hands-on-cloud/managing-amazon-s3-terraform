1a4ab9c24da768070159104dbbd2525d6b178433
subhasis020299 <subhasisdas125@gmail.com>
2022-05-27 15:49:35 +0530

    aded provider and data source to fetch state data

---
+++ s3_backend/ec2/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.15.1"
+    }
+  }
+
+  #specifying remote backend configuration:
+  backend "s3" {
+    bucket         = "terraform-remote-backend-20220527093846103400000001"
+    key            = "terraform.tfstate" #the file name with which state will get saved
+    region         = "ap-south-1"        #cant use variable here
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
