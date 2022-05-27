d18bf7aee8a874a6422d4bed176d2d088aa19b61
subhasis020299 <subhasisdas125@gmail.com>
2022-05-27 11:46:20 +0530

    added content_type to ensure html rendering

--- s3_website/s3.tf
+++ s3_website/s3.tf
@@ -41,6 +41,7 @@ resource "aws_s3_object" "object" {
   key    = "website/${each.value}"
   source = "uploads/${each.value}"
   etag = filemd5("uploads/${each.value}")
+  content_type = "text/html"
 
   depends_on = [
     aws_s3_bucket.bucket
